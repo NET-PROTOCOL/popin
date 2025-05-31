61225d3e898e7b907ab82d86fc5d92d72dc475f3
trieeun <ri2eeuntt@gmail.com>
2025-06-01 03:59:52 +0900

    입장선택기능추가

--- L3_timer.h
+++ L3_timer.h
@@ -1,6 +1,6 @@
-// L3_timer.h - Enhanced timer functionality
+// L3_timer.h 
 void L3_timer_startTimer();
 void L3_timer_stopTimer();
 uint8_t L3_timer_getTimerStatus();
-void L3_timer_boothSelectionStart();  // New function for booth selection timer
-void L3_timer_boothSelectionStop();   // New function
+void L3_timer_boothSelectionStart(); 
+void L3_timer_boothSelectionStop();   
